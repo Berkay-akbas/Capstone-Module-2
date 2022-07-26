eaef7e76165d02d8d5710e5062716edb22cd746a
gak92 <44802922+gak92@users.noreply.github.com>
2022-07-26 18:02:17 +0500

    Add functionality for comment popup window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const openComment = document.querySelector('#open-comment');
+const modalContainer = document.querySelector('#modal_container');
+const closeComment = document.querySelector('#close-comment');
+const modal = document.querySelector('.modal');
+
+openComment.addEventListener('click', () => {
+  modalContainer.classList.add('show');
+});
+
+closeComment.addEventListener('click', () => {
+  modalContainer.classList.remove('show');
+});
