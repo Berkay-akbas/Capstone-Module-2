0b0206a80968e8888d90c0fb17a18d52ddf91f3f
Berkay Akbas <berkayakbas@gmail.com>
2022-07-26 15:54:17 +0300

    Fix lintern

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 const asyncGetCall = async () => {
   try {
-    const username = 'gerandeklerk'
+    const username = 'gerandeklerk';
     const response = await fetch(`https://api.unsplash.com/users/${username}/photos?orientation=portrait&page=1&per_page=9&client_id=ORV6G4HML0wf1fJObg3OgNJTNx-asI0llrIPLPMpwYw
     `, {
       method: 'GET',
@@ -10,23 +10,20 @@ const asyncGetCall = async () => {
     });
     const data = await response.json();
     return data;
-    
   } catch (error) {
     return error;
-  };
+  }
 };
-
 
 const photoWrapper = document.querySelector('.photos-wrapper');
 
-
 const showCars = () => {
- let dat = asyncGetCall().then((value) => {
-  for (let index = 0; index < value.length - 1; index++) {
-    const url = value[index].urls.thumb;
-  const b = document.createElement('div');
-  b.classList.add('photo-item');
-  b.innerHTML = `<img src="${url}" alt="photo" />
+  asyncGetCall().then((value) => {
+    for (let index = 0; index < value.length - 1; index += 1) {
+      const url = value[index].urls.thumb;
+      const b = document.createElement('div');
+      b.classList.add('photo-item');
+      b.innerHTML = `<img src="${url}" alt="photo" />
   <h3>Photo 1</h3>
   <div class="like-container">
     <i class="fa fa-heart"></i>
@@ -34,9 +31,8 @@ const showCars = () => {
   </div>
   <button>Comments</button>
   <button>Reservations</button>`;
-  photoWrapper.appendChild(b);
-  }
-    
+      photoWrapper.appendChild(b);
+    }
   });
 };
 
