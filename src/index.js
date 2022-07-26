8721b3062e87f37a41222aa7cd7171cc9874e5ab
Berkay Akbas <berkayakbas@gmail.com>
2022-07-26 15:46:14 +0300

    Add basic structure of main section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './style.css';
+
+const asyncGetCall = async () => {
+  try {
+    const username = 'gerandeklerk'
+    const response = await fetch(`https://api.unsplash.com/users/${username}/photos?orientation=portrait&page=1&per_page=9&client_id=ORV6G4HML0wf1fJObg3OgNJTNx-asI0llrIPLPMpwYw
+    `, {
+      method: 'GET',
+
+    });
+    const data = await response.json();
+    return data;
+    
+  } catch (error) {
+    return error;
+  };
+};
+
+
+const photoWrapper = document.querySelector('.photos-wrapper');
+
+
+const showCars = () => {
+ let dat = asyncGetCall().then((value) => {
+  for (let index = 0; index < value.length - 1; index++) {
+    const url = value[index].urls.thumb;
+  const b = document.createElement('div');
+  b.classList.add('photo-item');
+  b.innerHTML = `<img src="${url}" alt="photo" />
+  <h3>Photo 1</h3>
+  <div class="like-container">
+    <i class="fa fa-heart"></i>
+    <div class="like-counter">4</div>
+  </div>
+  <button>Comments</button>
+  <button>Reservations</button>`;
+  photoWrapper.appendChild(b);
+  }
+    
+  });
+};
+
+showCars();
