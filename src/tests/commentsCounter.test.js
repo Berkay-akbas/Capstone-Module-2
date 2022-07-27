cd3998c08a3ede63cef66b67a50717dd3071c8fe
gak92 <44802922+gak92@users.noreply.github.com>
2022-07-27 21:01:13 +0500

    Add test case to count number of comments

---
+++ src/tests/commentsCounter.test.js
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { countComments } from "../comments";
+
+describe("Testing Number of comments", () => {
+  test("Test 3 comment in list", () => {
+    const comments = [
+      {
+          "creation_date": "2022-07-26",
+          "comment": "I love this red car",
+          "username": "Jane"
+      },
+      {
+          "creation_date": "2022-07-26",
+          "comment": "This car is awesome for long drive",
+          "username": "John"
+      },
+      {
+          "comment": "What a lovely car",
+          "username": "Brad",
+          "creation_date": "2022-07-27"
+      }
+    ];
+  
+    const numberOfComment = countComments(comments);
+    expect(numberOfComment).toBe(3);
+  });
+
+  test("Test 0 comment", () => {
+    const comments = [];
+    const numberOfComment = countComments(comments);
+    expect(numberOfComment).toBe(0);
+  });
+});
