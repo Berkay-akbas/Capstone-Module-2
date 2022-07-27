b6d26368c5c5b4d6f45c3094a0756f7e5778c8d2
gak92 <44802922+gak92@users.noreply.github.com>
2022-07-27 15:01:29 +0500

    Add async function to post comment via API

--- src/comments.js
+++ src/comments.js
@@ -13,4 +13,29 @@ const getComments = async (id) => {
   }
 };
 
-export default getComments;
+const postComments = async (id, userName, userComment) => {
+  try {
+    const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+    const appID = 'os4g1sp0qRngry7c8HYv';
+    const requestedURL = `${baseURL}/apps/${appID}/comments`;
+
+    const response = await fetch(requestedURL, {
+      method: 'POST',
+      body: JSON.stringify({
+        "item_id": id,
+        "username": userName,
+        "comment": userComment,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    const data = await response;
+    return data.status;
+  } catch (error) {
+    return error;
+  }
+};
+
+export { getComments, postComments} ;
